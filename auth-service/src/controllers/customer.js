de604ce31381c372c9bd631c8065073a6b6aa03c
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-08-15 15:19:25 +0530

    create login routes

---
+++ auth-service/src/controllers/customer.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser');
+
+const customerLogginController = (req, res) => {
+    const {email, password} = req.body;
+    
+
+}
+
+module.exports = {customerLogginController};
--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const {customerRouter} = require('./routes/customer');
 
+//app is an instance of express
 const app = express();
 app.use(cors());
-
+app.use(bodyParser.json());
 const PORT = 3002;
 
-app.get('/', (req, res) => {
-    res.send('Hello World from auth-services');
-});
+//define the route for the customer
+app.use('/customer', customerRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-}   );
+});
