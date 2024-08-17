2ca9073e717f052ed7fcdc3011195ed31fe624e9
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-08-17 17:45:57 +0530

    get bills by customer id is completed

--- customer-service/src/controllers/bills.js
+++ customer-service/src/controllers/bills.js
@@ -1,53 +1,68 @@
-const asyncHandler = require('express-async-handler');
-const jwt = require('jsonwebtoken');
-const db = require('../config/db');
-
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const db = require("../config/db");
 
 // Get all bills by customer id (any business any branch)
 const getAllBillsByCustomerId = asyncHandler(async (req, res) => {
-    const customer_id = req.customer.customer_id;   
+  const customer_id = req.customer.customer_id;
 
-    const getBillsQuery = `SELECT bill_id, branch_id, date_time, total_price FROM bill WHERE customer_id = ?`;
+  const getBillsQuery = `
+    SELECT 
+        b.bill_id, 
+        bus.business_id, 
+        b.date_time, 
+        b.total_price 
+    FROM 
+        bill b 
+    JOIN 
+        business_branch bb ON b.branch_id = bb.branch_id 
+    JOIN 
+        business bus ON bb.business_id = bus.business_id 
+    WHERE 
+        b.customer_id = ?;
+`;
 
-    db.query(getBillsQuery, [customer_id], (err, results) => {
-        if (err) {
-            return res.status(500).json({ message: err.message });
-        } else {
-            return res.status(200).json(results);
-        }
+  db.query(getBillsQuery, [customer_id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    } else {
+      return res.status(200).json(results);
     }
-    );
+  });
 });
-
 
 // Get infomation of bills by bill id
 const getBillByID = asyncHandler(async (req, res) => {
-    const bill_id = req.params.billID;
+  const bill_id = req.params.billID;
 
-    const getBillQuery = `SELECT * FROM bill WHERE bill_id = ?`;
+  const getBillQuery = `SELECT * FROM bill WHERE bill_id = ?`;
 
-    db.query(getBillQuery, [bill_id], (err, results) => {
-        if (err) {
-            return res.status(500).json({ message: err.message });
-        } else {
-            return res.status(200).json(results);
-        }
-    });
+  db.query(getBillQuery, [bill_id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    } else {
+      return res.status(200).json(results);
+    }
+  });
 });
 
 // Get all bills by business id
 const getAllBillsByBusinessID = asyncHandler(async (req, res) => {
-    const business_id = req.params.businessID;
+  const business_id = req.params.businessID;
 
-    const getBillsQuery = `SELECT bill_id, date_time, total_price FROM bill WHERE branch_id = ?`;
+  const getBillsQuery = `SELECT bill_id, date_time, total_price FROM bill WHERE branch_id = ?`;
 
-    db.query(getBillsQuery, [business_id], (err, results) => {
-        if (err) {
-            return res.status(500).json({ message: err.message });
-        } else {
-            return res.status(200).json(results);
-        }
-    });
+  db.query(getBillsQuery, [business_id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    } else {
+      return res.status(200).json(results);
+    }
+  });
 });
 
-module.exports = { getAllBillsByCustomerId, getBillByID, getAllBillsByBusinessID };
+module.exports = {
+  getAllBillsByCustomerId,
+  getBillByID,
+  getAllBillsByBusinessID,
+};
