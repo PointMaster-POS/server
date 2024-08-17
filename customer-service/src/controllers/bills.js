fa94f041e9d64b9cdd0b95ed15a5897084e675d4
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-08-17 18:08:32 +0530

    all controllers related to the bills are created

--- customer-service/src/controllers/bills.js
+++ customer-service/src/controllers/bills.js
@@ -49,10 +49,26 @@ const getBillByID = asyncHandler(async (req, res) => {
 // Get all bills by business id
 const getAllBillsByBusinessID = asyncHandler(async (req, res) => {
   const business_id = req.params.businessID;
+  const customer_id = req.customer.customer_id;
+  const query = `SELECT 
+  b.bill_id, 
+  b.date_time, 
+  b.total_price, 
+  bb.branch_id, 
+  bus.business_id
+FROM 
+  bill b
+JOIN 
+  business_branch bb ON b.branch_id = bb.branch_id
+JOIN 
+  business bus ON bb.business_id = bus.business_id
+WHERE 
+  b.customer_id = ? 
+AND 
+  bus.business_id = ?`;
 
-  const getBillsQuery = `SELECT bill_id, date_time, total_price FROM bill WHERE branch_id = ?`;
 
-  db.query(getBillsQuery, [business_id], (err, results) => {
+  db.query(query, [customer_id,business_id], (err, results) => {
     if (err) {
       return res.status(500).json({ message: err.message });
     } else {
