7a1108572d9f4a6b97e73a5a5546289d97684d23
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-08-17 10:46:37 +0530

    change database configuration

--- auth-service/src/config/db.js
+++ auth-service/src/config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'logintest'
+    database: 'pointmaster'
 });
 
 db.connect((err) => {
