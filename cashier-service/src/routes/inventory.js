db333a2da7b87a1f3bcb569313c7a94d4913bc1c
Himindu Kularathne <143618014+Himindu-Kularathne@users.noreply.github.com>
2024-08-21 11:10:06 +0530

    define inventory routes

--- cashier-service/src/routes/inventory.js
+++ cashier-service/src/routes/inventory.js
@@ -1,2 +1,15 @@
 //get all categories related to the business 
 //get all products related to the category
+
+
+const express = require('express');
+const inventoryRouter = express.Router();
+
+const { getCategories, getProducts} = require('../controllers/loyality');
+
+//get all categories program of the business
+//this route should be protected by the auth middleware cashier should be authenticated
+inventoryRouter.get('/categories', getCategories);
+inventoryRouter.get('/products/:categoryID', getProducts);
+
+module.exports = inventoryRouter;
