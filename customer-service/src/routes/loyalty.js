55fc6d2aea5ea723a38c073907bbcb7038e321c9
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-09-15 13:49:40 +0530

    customer routes integrated with swagger

--- customer-service/src/routes/loyalty.js
+++ customer-service/src/routes/loyalty.js
@@ -1,29 +1,134 @@
 const express = require("express");
 const loyalityRouter = express.Router();
-
 const {
   getLoyalityPointsByBusinessID,
-  //   redeemLoyalityPointsAtShop,
+  // redeemLoyalityPointsAtShop,
   getLoyalityPointsByCustomerID,
 } = require("../controllers/loyality");
 const validateToken = require("../middleware/validateTokenHandler");
 
-//this should return all the loyality points of a customer for a business
-//protected route
-loyalityRouter.get(
-  "/:businessID",
-  validateToken,
-  getLoyalityPointsByBusinessID
-);
+/**
+ * @swagger
+ * tags:
+ *   name: Loyalty
+ *   description: Operations related to loyalty points
+ */
 
-//this should redeem loyality points of a customer for business , when customer at the shop and wants to redeem points
-//this process will be done at the cashier so the cashier will enter the customer id and business id and the points to be redeemed
-//should be call from customer service
-// loyalityRouter
-//   .route("/redeem/:customerID/:businessID")
-//   .post(redeemLoyalityPointsAtShop);
+/**
+ * @swagger
+ * /loyalty/{businessID}:
+ *   get:
+ *     summary: Get loyalty points by business ID
+ *     description: Returns all the loyalty points of a customer for a specified business.
+ *     tags:
+ *       - Loyalty
+ *     parameters:
+ *       - in: path
+ *         name: businessID
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the business
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved loyalty points
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   customerID:
+ *                     type: string
+ *                     description: The ID of the customer
+ *                   points:
+ *                     type: integer
+ *                     description: The amount of loyalty points
+ *       401:
+ *         description: Unauthorized - Authentication required
+ *       500:
+ *         description: Internal Server Error
+ */
+loyalityRouter.get("/:businessID", validateToken, getLoyalityPointsByBusinessID);
 
-//this should return all the loyality points of a customer , should be called from cashier or manager
+/**
+ * @swagger
+ * /loyalty:
+ *   get:
+ *     summary: Get loyalty points by customer ID
+ *     description: Returns all the loyalty points of a customer. This route requires authentication.
+ *     tags:
+ *       - Loyalty
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved loyalty points
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   businessID:
+ *                     type: string
+ *                     description: The ID of the business
+ *                   points:
+ *                     type: integer
+ *                     description: The amount of loyalty points
+ *       401:
+ *         description: Unauthorized - Authentication required
+ *       500:
+ *         description: Internal Server Error
+ */
 loyalityRouter.get("/", validateToken, getLoyalityPointsByCustomerID);
+
+// Uncomment and update the following route if it will be implemented in the future
+// /**
+//  * @swagger
+//  * /loyalty/redeem/{customerID}/{businessID}:
+//  *   post:
+//  *     summary: Redeem loyalty points at shop
+//  *     description: Redeem loyalty points of a customer for a business at the shop.
+//  *     tags:
+//  *       - Loyalty
+//  *     parameters:
+//  *       - in: path
+//  *         name: customerID
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *         description: The ID of the customer
+//  *       - in: path
+//  *         name: businessID
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *         description: The ID of the business
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             properties:
+//  *               points:
+//  *                 type: integer
+//  *                 description: The number of points to be redeemed
+//  *     responses:
+//  *       200:
+//  *         description: Points redeemed successfully
+//  *       400:
+//  *         description: Bad Request - Invalid input
+//  *       401:
+//  *         description: Unauthorized - Authentication required
+//  *       500:
+//  *         description: Internal Server Error
+//  */
+// loyalityRouter.post("/redeem/:customerID/:businessID", redeemLoyalityPointsAtShop);
 
 module.exports = loyalityRouter;
