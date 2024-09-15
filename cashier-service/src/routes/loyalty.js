df7c447b7c0fd491d5895815290e9004ab00fda6
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-09-15 12:47:03 +0530

    swagger integration for customers loyality routes

--- cashier-service/src/routes/loyalty.js
+++ cashier-service/src/routes/loyalty.js
@@ -1,13 +1,53 @@
-//get loyality program of the business
-
 const express = require('express');
 const loyaltyRouter = express.Router();
-const {getLoyalityProgram} = require('../controllers/loyalty');
+const { getLoyalityProgram } = require('../controllers/loyalty');
 const verifyCashier = require('../middleware/verifyCashier');
-const {businessAccess, branchAccess} = require('../middleware/businessAccess');
+const { businessAccess, branchAccess } = require('../middleware/businessAccess');
 
-//get looyalty program details of the business 
-//this route should be protected by the auth middleware cashier should be authenticated
-loyaltyRouter.get('/',verifyCashier, branchAccess, businessAccess,getLoyalityProgram);
+
+/**
+ * @swagger
+ * /cashier/loyalty:
+ *   get:
+ *     summary: Get Loyalty Program Details of the Business
+ *     description: Retrieves the details of the loyalty program for the business. This route is protected and requires authentication.
+ *     tags:
+ *       - Loyalty
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved loyalty program details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 programId:
+ *                   type: string
+ *                   description: The unique identifier of the loyalty program
+ *                 programName:
+ *                   type: string
+ *                   description: The name of the loyalty program
+ *                 description:
+ *                   type: string
+ *                   description: Description of the loyalty program
+ *                 benefits:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     description: List of benefits provided by the loyalty program
+ *       401:
+ *         description: Unauthorized - Authentication required
+ *       403:
+ *         description: Forbidden - Access denied
+ *       500:
+ *         description: Internal Server Error
+ */
+
+
+
+// Get loyalty program details of the business
+loyaltyRouter.get('/', verifyCashier, branchAccess, businessAccess, getLoyalityProgram);
 
 module.exports = loyaltyRouter;
