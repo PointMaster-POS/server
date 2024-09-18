08fa585484660ae45c293e4df832e57963af37b1
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-09-18 08:21:07 +0530

    auth service updated to business owners login

--- admin-service/src/routes/category.js
+++ admin-service/src/routes/category.js
@@ -1,54 +1,52 @@
-const categoryRouter = require("express").Router();
-const validateToken = require("../middleware/validateToken");
-const {
-  createCategory,
-} = require("../controllers/category");
-const { branchAccess } = require("../middleware/businessAccess");
+// const categoryRouter = require("express").Router();
+// const validateToken = require("../middleware/validateToken");
+// const {createCategory, gatALLCategoryInventoryManager, updateCategoryInventoryManager, deleteCategoryInventoryManager, createAllCategoryInventoryManager} = require("../controllers/category");
+// const { branchAccess } = require("../middleware/businessAccess");
 
-/**
- * @swagger
- * /category/manager:
- *   post:
- *     summary: Create a category by the inventory manager
- *     tags:
- *       - Category
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               category_name:
- *                 type: string
- *                 description: The name of the category
- *                 example: "Electronics"
- *               category_location:
- *                 type: string
- *                 description: Category description
- *                 example: "3rd floor"
- *     responses:
- *       200:
- *         description: Category created successfully
- *       401:
- *         description: Unauthorized
- */
-//if manager the branch_id is in the token if owner the branch_id is in the body
-// categoryRouter.post("/manager", validateToken, branchAccess, createCategory);
-categoryRouter.post("/owner", validateToken, createCategory);
+// /**
+//  * @swagger
+//  * /category/manager:
+//  *   post:
+//  *     summary: Create a category by the inventory manager
+//  *     tags:
+//  *       - Category
+//  *     security:
+//  *       - bearerAuth: []
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             properties:
+//  *               category_name:
+//  *                 type: string
+//  *                 description: The name of the category
+//  *                 example: "Electronics"
+//  *               category_location:
+//  *                 type: string
+//  *                 description: Category description
+//  *                 example: "3rd floor"
+//  *     responses:
+//  *       200:
+//  *         description: Category created successfully
+//  *       401:
+//  *         description: Unauthorized
+//  */
+// //if manager the branch_id is in the token if owner the branch_id is in the body
+// // categoryRouter.post("/manager", validateToken, branchAccess, createCategory);
+// categoryRouter.post("/owner", validateToken, createAllCategoryInventoryManager);
 
-categoryRouter.get("/owner/:branch_id", validateToken, gatALLCategoryInventoryManager);
-//branch id is in the token if manager is creating item
-categoryRouter.get("/manager", validateToken, branchAccess, gatALLCategoryInventoryManager);
+// categoryRouter.get("/owner/:branch_id", validateToken, gatALLCategoryInventoryManager);
+// //branch id is in the token if manager is creating item
+// categoryRouter.get("/manager", validateToken, branchAccess, gatALLCategoryInventoryManager);
 
-// categoryRouter.put("/manager", validateToken, branchAccess, updateCategoryInventoryManager);
-categoryRouter.put("/owner/branch_id", validateToken, updateCategoryInventoryManager);
+// // categoryRouter.put("/manager", validateToken, branchAccess, updateCategoryInventoryManager);
+// categoryRouter.put("/owner/branch_id", validateToken, updateCategoryInventoryManager);
 
 
-// categoryRouter.delete("/manager", validateToken, branchAccess, deleteCategoryInventoryManager);
-categoryRouter.delete("/owner/:branch_id", validateToken, deleteCategoryInventoryManager);
+// // categoryRouter.delete("/manager", validateToken, branchAccess, deleteCategoryInventoryManager);
+// categoryRouter.delete("/owner/:branch_id", validateToken, deleteCategoryInventoryManager);
 
 
-module.exports = categoryRouter;
+// module.exports = categoryRouter;
--- auth-service/src/controllers/employee.js
+++ auth-service/src/controllers/employee.js
@@ -1,64 +1,171 @@
 const asyncHandler = require("express-async-handler");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const db = require('../config/db');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../config/db");
 
 const employeeLogginController = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-
-    if (!email || !password) {
-      return res.status(400).json({ message: "Email and password are required" });
-    }
-    db.query("SELECT * FROM employee WHERE employee_email = ?", [email], async (err, result) => {
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  db.query(
+    "SELECT * FROM employee WHERE employee_email = ?",
+    [email],
+    async (err, result) => {
       if (err) {
         res.status(400).json(err);
         console.log(err);
       } else {
         if (result.length === 0) {
-          res.status(401).json({ message: "Invalid email or password" });
+          db.query(
+            "SELECT * FROM employee WHERE employee_name = ?",
+            [email],
+            async (err, result) => {
+              if (err) {
+                res.status(400).json(err);
+                console.log(err);
+              } else {
+                db.query(
+                  "SELECT * from business Where business_owner_mail = ?",
+                  [email],
+                  async (err, result) => {
+                    if (err) {
+                      res.status(400).json(err);
+                      console.log(err);
+                    } else {
+                      const business = result[0];
+                      console.log(business);
+                      let isMatch = false;
+                      if (!business) {
+                        db.query(
+                          "SELECT * from business Where business_mail = ?",
+                          [email],
+                          async (err, result) => {
+                            if (err) {
+                              res.status(400).json(err);
+                              console.log(err);
+                            } else {
+                              const business = result[0];
+                              console.log(business);
+                              let isMatch = false;
+                              if (!business) {
+                                return res
+                                  .status(401)
+                                  .json({
+                                    message: "Invalid email or password",
+                                  });
+                              } else {
+                                console.log({ business: business });
+                                try {
+                                  isMatch = await bcrypt.compare(
+                                    password,
+                                    business.password
+                                  );
+                                  if (isMatch) {
+                                    const accessToken = jwt.sign(
+                                      {
+                                        business: {
+                                          business_name: business.business_name,
+                                          business_id: business.business_id,
+                                          email: business.business_owner_mail,
+                                        },
+                                      },
+                                      process.env.ACCESS_TOKEN_SECRET,
+                                      { expiresIn: "30m" }
+                                    );
+                                    res.status(200).json(accessToken);
+                                  } else {
+                                    res
+                                      .status(401)
+                                      .json({
+                                        message: "Invalid email or password",
+                                      });
+                                  }
+                                } catch (error) {
+                                  console.log(error);
+                                  res
+                                    .status(500)
+                                    .json({ message: "Internal server error" });
+                                }
+                              }
+                            }
+                          }
+                        );
+                      } else {
+                        console.log({ business: business });
+                        try {
+                          isMatch = await bcrypt.compare(
+                            password,
+                            business.password
+                          );
+                          if (isMatch) {
+                            const accessToken = jwt.sign(
+                              {
+                                business: {
+                                  business_name: business.business_name,
+                                  business_id: business.business_id,
+                                  email: business.business_owner_mail,
+                                },
+                              },
+                              process.env.ACCESS_TOKEN_SECRET,
+                              { expiresIn: "30m" }
+                            );
+                            res.status(200).json(accessToken);
+                          } else {
+                            res
+                              .status(401)
+                              .json({ message: "Invalid email or password" });
+                          }
+                        } catch (error) {
+                          console.log(error);
+                          res
+                            .status(500)
+                            .json({ message: "Internal server error" });
+                        }
+                      }
+                    }
+                  }
+                );
+              }
+            }
+          );
         }
-        
+
         const employee = result[0];
         console.log(employee);
         let isMatch = false;
 
-
         if (!employee) {
           return res.status(401).json({ message: "Invalid email or password" });
-        }
-        else {
-          console.log({employee: employee});
+        } else {
+          console.log({ employee: employee });
 
-        try {
-          isMatch = await bcrypt.compare(password, employee.password);
-          if(isMatch) {
-            const accessToken = jwt.sign(
-              {
-                employee: {
-                  employee_name: employee.employee_name,
-                  employee_id: employee.employee_id,
-                  employee_role: employee.role,
-                  email: employee.email,
+          try {
+            isMatch = await bcrypt.compare(password, employee.password);
+            if (isMatch) {
+              const accessToken = jwt.sign(
+                {
+                  employee: {
+                    employee_name: employee.employee_name,
+                    employee_id: employee.employee_id,
+                    employee_role: employee.role,
+                    email: employee.email,
+                  },
                 },
-              },
-              process.env.ACCESS_TOKEN_SECRET,
-              { expiresIn: "30m" }
-            );
-            res.status(200).json(accessToken);
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "30m" }
+              );
+              res.status(200).json(accessToken);
+            } else {
+              res.status(401).json({ message: "Invalid email or password" });
+            }
+          } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Internal server error" });
           }
-          else {
-            res.status(401).json({ message: "Invalid email or password" });
-          }
-        } catch (error) {
-          console.log(error);
-          res.status(500).json({ message: "Internal server error" });
-
         }
-      }
 
-        
-        
         // if (isMatch) {
         //   const accessToken = jwt.sign(
         //     {
@@ -73,23 +180,20 @@ const employeeLogginController = asyncHandler(async (req, res) => {
         //     { expiresIn: "30m" }
         //   );
 
-          // const refreshToken = jwt.sign(
-          //   {
-          //     employee: {
-          //       employee_name: employee.employee_name,
-          //       employee_id: employee.employee_id,
-          //       email: employee.email,
-          //     },
-          //   },
-          //   "panadura",
-          //   { expiresIn: "120m" }
-          // );
-  
-       
+        // const refreshToken = jwt.sign(
+        //   {
+        //     employee: {
+        //       employee_name: employee.employee_name,
+        //       employee_id: employee.employee_id,
+        //       email: employee.email,
+        //     },
+        //   },
+        //   "panadura",
+        //   { expiresIn: "120m" }
+        // );
       }
     }
   );
 });
 
-
-module.exports = {employeeLogginController};
+module.exports = { employeeLogginController };
