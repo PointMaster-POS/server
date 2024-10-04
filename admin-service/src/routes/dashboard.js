c6983549c52029efb50f29e6c58d14736de3fbbd
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-10-04 23:37:43 +0530

    add low stock endpoint

--- admin-service/src/routes/dashboard.js
+++ admin-service/src/routes/dashboard.js
@@ -1,19 +1,66 @@
-const dashBoardRouter = require('express').Router();
-const validateToken = require('../middleware/validateToken');
+const dashBoardRouter = require("express").Router();
+const validateToken = require("../middleware/validateToken");
 
+const {
+  getPopularItems,
+  getExpiredItems,
+  getNumberOfBillsPerMonth,
+  getServiceTimeReport,
+  getNumberOfCustomers,
+  customerProfileWithMostPoints,
+  salesByPaymentMethod,
+  getBillsBetweenDates,
+  getBranchPerformanceReport,
+  getItemsWithLowStock
+} = require("../controllers/dashboard");
 
-const { getPopularItems, getExpiredItems, getNumberOfBillsPerMonth , getServiceTimeReport, getNumberOfCustomers,  customerProfileWithMostPoints, salesByPaymentMethod} = require('../controllers/dashboard');
+dashBoardRouter.get(
+  "/business/sale-report/item/:startDate/:endDate",
+  validateToken,
+  getPopularItems
+);
+dashBoardRouter.get("/business/expired-items", validateToken, getExpiredItems);
+dashBoardRouter.get(
+  "/business/sale-report/number-of-bills/:startMonth/:endMonth",
+  validateToken,
+  getNumberOfBillsPerMonth
+);
+dashBoardRouter.get(
+  "/business/sale-report/service-time/:startDate/:endDate",
+  validateToken,
+  getServiceTimeReport
+);
+dashBoardRouter.get(
+  "/business/number-of-customers",
+  validateToken,
+  getNumberOfCustomers
+);
+dashBoardRouter.get(
+  "/business/customer-profile-with-most-points",
+  validateToken,
+  customerProfileWithMostPoints
+);
+dashBoardRouter.get(
+  "/business/sales-by-payment-method",
+  validateToken,
+  salesByPaymentMethod
+);
+dashBoardRouter.get(
+  "/business/bills-between-dates/:startDate/:endDate",
+  validateToken,
+  getBillsBetweenDates
+);
 
+dashBoardRouter.get(
+  "/business/branch-performance/:startDate/:endDate",
+  validateToken,
+  getBranchPerformanceReport
+);
 
-dashBoardRouter.get('/business/sale-report/item/:startDate/:endDate', validateToken, getPopularItems);
-dashBoardRouter.get('/business/expired-items', validateToken, getExpiredItems);
-dashBoardRouter.get('/business/sale-report/number-of-bills/:startMonth/:endMonth', validateToken, getNumberOfBillsPerMonth);
-dashBoardRouter.get('/business/sale-report/service-time/:startDate/:endDate', validateToken, getServiceTimeReport);
-dashBoardRouter.get('/business/number-of-customers', validateToken, getNumberOfCustomers);
-dashBoardRouter.get('/business/customer-profile-with-most-points', validateToken, customerProfileWithMostPoints);
-dashBoardRouter.get('/business/sales-by-payment-method', validateToken, salesByPaymentMethod);
-
-
-
+dashBoardRouter.get(
+    "/business/low-stock-items/:minStock",
+    validateToken,
+    getItemsWithLowStock
+);
 
 module.exports = dashBoardRouter;
